42075dcf78412b846c8a8eb1083697492e61b20f
Ephriam Kassa <ephriam123@gmail.com>
2024-06-01 21:12:44 -0700

    dsadsadasdsad

--- src/App.jsx
+++ src/App.jsx
@@ -58,8 +58,8 @@ function App() {
   return (
     <div className="main-container">
       <nav className="navbar">
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Pump.Fun </a>
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Telegram</a>
+        <a href="https://ufepepe.netlify.app/" target="_blank" rel="noopener noreferrer">Pump.Fun </a>
+        <a href="https://t.me/PepeUFCPortal" target="_blank" rel="noopener noreferrer">Telegram</a>
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">DEX</a>
       </nav>
       <h1 className="main-title">PEPE UFC</h1>
